2e35f0feb42582e7b0b7272cfc0bf8229f495fac
DarrenDD <88164188+DarrenDD@users.noreply.github.com>
2022-01-16 17:13:37 +0200

    Add project files.

---
+++ PermissionManagement/Constants/Roles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PermissionManagement.Constants
+{
+    public enum Roles
+    {
+        SuperAdmin,
+        Admin,
+        Student, 
+        Teacher,
+        CentreManager
+    }
+}
